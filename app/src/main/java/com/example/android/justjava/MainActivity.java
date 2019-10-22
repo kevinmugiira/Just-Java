585ace0a0fc0283302fba830fcc27d7ef84bb299
kevinmugiira <kevinm5740@aol.com>
2019-10-22 16:37:20 +0300

    updated the increament and decrement buttons

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.android.justjava;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  *The submitOrder method and the createOrderSummary method have under gone various changes, please follow keenly
@@ -34,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void submitOrder(View view) {
 
+        int howMuch = qunatity * 150 ;
+
         //handles the edit text
         EditText  edtext = (EditText) findViewById(R.id.name_editText);
         String jina = edtext.getText().toString();
@@ -41,12 +45,18 @@ public class MainActivity extends AppCompatActivity {
         //handles the whipped cream checkbox
         CheckBox cb = (CheckBox) findViewById(R.id.whipped_cream_checkBox);
         boolean hasWhippedCream = cb.isChecked();
+        if (hasWhippedCream){
+            howMuch = howMuch + 50;
+        }
 
         //handles the chocolate checkbox
         CheckBox checkB = (CheckBox) findViewById(R.id.chocolate_checkBox);
         boolean hasChocolate = checkB.isChecked();
+        if (hasChocolate){
+            howMuch = howMuch + 50;
+        }
 
-        int howMuch = qunatity * 150 ;
+
         //String priceMessage = "Amount:ksh " + price;
         //priceMessage = "Name: Lyla Ali\nAdded Whipped cream: "+ hasWhippedCream +"\nQuantity:"+ qunatity + "\n"+ priceMessage;
         //priceMessage = priceMessage + "\nThankYou, Come again!";
@@ -72,21 +82,88 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      *This method is called when the increment button is pressed
+     * A toast is used to notify the maximum limit
      */
     public void increment(View view) {
         qunatity = qunatity + 1;
+        if (qunatity > 100){
+
+            qunatity = 100;
+
+            //Toast
+            Context context = getApplicationContext();
+            CharSequence text = "Can not order more than 100";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+            return;
+        }
         displayQuantity(qunatity);
     }
 
 
     /**
+     *         Or
+     *
+     * public void increment(View view) {
+     *
+     *         if (qunatity == 100){
+     *
+     *             //Toast
+     *             Context context = getApplicationContext();
+     *             CharSequence text = "Can not order more than 100";
+     *             int duration = Toast.LENGTH_SHORT;
+     *
+     *             Toast toast = Toast.makeText(context, text, duration);
+     *             toast.show();
+     *
+     *             return;
+     *         }
+     *         qunatity = qunatity + 1;
+     *         displayQuantity(qunatity);
+     *     }
+     */
+
+
+    /**
      * This method is called when the decrement button is pressed
+     * A toast is used to notify the minimum limit
      */
 
     public void decrement(View view) {
         qunatity = qunatity - 1;
+        if (qunatity < 1){
+            qunatity = 1;
+
+            //Toast
+            Context context = getApplicationContext();
+            CharSequence text = "Can not order less than 1";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+            return;
+        }
         displayQuantity(qunatity);
     }
+
+
+    /**
+     *        Or
+     *
+     * public void decreament (View view) {
+     *     if (qunatity ==1){
+     *         //Toast
+     *         Toast.makeText(this, "Can not order less than 1", Toast.LENGTH_SHORT).show();
+     *         return;
+     *     }
+     *     qunatity = qunatity - 1;
+     *     displayQuantity(qunatity)
+     * }
+     */
 
 
 
